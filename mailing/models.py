f247ffc500e18c0a365fe9e9992b196aeb996d7f
kiril_tarasov <kirilltaras1792@yandex.ru>
2025-06-07 21:44:18 +0700

    Создать модель User и Client

--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    email = models.EmailField(unique=True, verbose_name="Email")
+    full_name = models.CharField(max_length=50, verbose_name="ФИО", null=True, blank=True)
+    comment = models.TextField(max_length=150, verbose_name="Комментарий", blank=True, null=True)
+
+    class Meta:
+        ordering = ['full_name']
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
+
+    def __str__(self):
+        return f'{self.full_name} <{self.email}>'
+
